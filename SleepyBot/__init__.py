fceca869c04829489077f38d5a374c8245bd549b
HitSyr <66272318+HitSyr@users.noreply.github.com>
2021-10-20 10:18:58 -0500

    Bump version to 2.1.2.
    This is only so the latest release of the v2.x includes the font licenses. No further updates will be coming to this branch as it is deprecated.

--- SleepyBot/__init__.py
+++ SleepyBot/__init__.py
@@ -10,7 +10,7 @@ A Discord bot for those who like to sleep.
 __title__ = "Sleepy"
 __author__ = "HitchedSyringe"
 __copyright__ = "Copyright (c) 2018-2020 HitchedSyringe"
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 
 
 from .bot import Sleepy
