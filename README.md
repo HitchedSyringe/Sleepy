e5ed4159fa765212aec441f5bf6c1b8cb37e73aa
HitchedSyringe <66272318+HitSyr@users.noreply.github.com>
2022-06-28 21:34:19 -0500

    Fix colouring & performance issues in `nichijou`.
    (Backported from local v3.3.0a branch).

--- README.md
+++ README.md
@@ -59,7 +59,7 @@ When adding commands, look at the other commands in the category and pick which 
 * [jishaku 2.2.0+](https://github.com/Gorialis/jishaku)
 * [matplotlib](https://github.com/matplotlib/matplotlib)
 * [opencv-python-headless](https://github.com/opencv/opencv-python)
-* [Pillow 8.0.0+](https://github.com/python-pillow/Pillow)
+* [Pillow 8.x & 9.1.0+](https://github.com/python-pillow/Pillow)
 * [psutil](https://github.com/giampaolo/psutil)
 * [pyfiglet](https://github.com/pwaller/pyfiglet)
 * [pyyaml](https://github.com/yaml/pyyaml)
--- sleepy/ext/weeb/frontend.py
+++ sleepy/ext/weeb/frontend.py
@@ -18,7 +18,7 @@ import textwrap
 import discord
 from discord import Colour, Embed, File
 from discord.ext import commands
-from PIL import UnidentifiedImageError
+from PIL import GifImagePlugin as GIP, UnidentifiedImageError
 from PIL.Image import DecompressionBombError
 from sleepy.converters import (
     ImageAssetConverter,
@@ -122,6 +122,25 @@ class Weeb(
         elif isinstance(error, (commands.BadArgument, commands.MaxConcurrencyReached)):
             await ctx.send(error)
             error.handled__ = True
+
+    def cog_load(self):
+        from PIL import __version__ as pillow_version
+
+        # This is proofed against .dev[x] versions and release candidates.
+        major, minor, *_ = pillow_version.split(".")
+
+        if int(major) >= 9 and int(minor) >= 1:
+            # This modifies PIL's GIF loading strategy on the global level.
+            # This needs to be done since `nichijou` breaks on Pillow 9.x.
+            # Unfortunately, this can only be done on Pillow 9.1+. Pillow
+            # 9.0 people are on their own on this front, since there's no
+            # other way to modify the GIF loading strategy otherwise.
+            self._original_gif_loading_strategy = GIP.LOADING_STRATEGY
+            GIP.LOADING_STRATEGY = GIP.LoadingStrategy.RGB_AFTER_DIFFERENT_PALETTE_ONLY
+
+    def cog_unload(self):
+        if hasattr(self, "_original_gif_loading_strategy"):
+            GIP.LOADING_STRATEGY = self._original_gif_loading_strategy
 
     @commands.command(aliases=("animeinfo",))
     @commands.bot_has_permissions(embed_links=True)
