HitchedSyringe/Sleepy

commit 4b57b4556466fe2db489565392f23d4ce735e6eb

Simplify several command typehints.
Many of these workarounds are no longer necessary.

sleepy/ext/moderation.py
@@ -136,7 +136,7 @@ class _CleanDaysPsuedoFlag(commands.Converter[int]):
 
 class MassbanFlags(commands.FlagConverter):
 
-    reason: Annotated[str, Reason] = commands.flag(default=_no_reason)
+    reason: str = commands.flag(converter=Reason, default=_no_reason)
     clean_days: commands.Range[int, 0, 7] = commands.flag(name="clean", default=1)
 
     startswith: Optional[Tuple[str, ...]] = None
@@ -280,8 +280,8 @@ class Moderation(commands.Cog):
     async def ban(
         self,
         ctx: GuildContext,
-        users: Annotated[
-            Sequence[Union[discord.Member, discord.User]], commands.Greedy[BannableUser]
+        users: commands.Greedy[
+            Annotated[Union[discord.Member, discord.User], BannableUser]
         ],
         clean_days: Annotated[int, Optional[_CleanDaysPsuedoFlag]] = 1,
         *,

sleepy/ext/fun.py
@@ -217,7 +217,7 @@ class PollView(View):
 
 class FigletFlags(commands.FlagConverter):
     font: Optional[Annotated[str, str.lower]] = None
-    text: str = commands.flag(converter=commands.clean_content(fix_channel_mentions=True))
+    text: Annotated[str, commands.clean_content(fix_channel_mentions=True)]
 
 
 class PollFlags(commands.FlagConverter):

sleepy/ext/images/frontend.py
@@ -51,7 +51,7 @@ def resolve_font_from_name(font_name: str) -> Path:
 
 
 class TTIFlags(commands.FlagConverter):
-    text: str = commands.flag(converter=commands.clean_content(fix_channel_mentions=True))
+    text: Annotated[str, commands.clean_content(fix_channel_mentions=True)]
     font_path: Path = commands.flag(
         name="font",
         converter=resolve_font_from_name,
@@ -107,7 +107,7 @@ class Images(
         ctx: SleepyContext,
         inverted: Annotated[bool, Optional[_positional_bool_flag("-invert")]] = False,  # type: ignore
         *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        image: Annotated["PartialAsset", ImageAssetConverter],
     ) -> None:
         """Converts an image into ASCII art.
 
@@ -170,7 +170,7 @@ class Images(
             bool, Optional[_positional_bool_flag("-rebranded")]  # type: ignore
         ] = False,
         *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        image: Annotated["PartialAsset", ImageAssetConverter],
     ) -> None:
         """Blurplefies an image.
 
@@ -225,7 +225,7 @@ class Images(
     async def captcha(
         self,
         ctx: SleepyContext,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        image: Annotated["PartialAsset", ImageAssetConverter],
         *,
         text: Annotated[str, commands.clean_content(fix_channel_mentions=True)],
     ) -> None:
@@ -313,10 +313,7 @@ class Images(
     @commands.bot_has_permissions(attach_files=True)
     @commands.max_concurrency(5, commands.BucketType.guild)
     async def dalgona(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """\N{IDEOGRAPHIC NUMBER ZERO}\N{WHITE UP-POINTING TRIANGLE}\N{BALLOT BOX}
 
@@ -344,10 +341,7 @@ class Images(
     @commands.bot_has_permissions(attach_files=True)
     @commands.max_concurrency(5, commands.BucketType.guild)
     async def deepfry(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Deep fries an image.
 
@@ -426,10 +420,7 @@ class Images(
     @commands.bot_has_permissions(attach_files=True)
     @commands.max_concurrency(5, commands.BucketType.guild)
     async def invert(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Inverts an image's colours.
 
@@ -457,10 +448,7 @@ class Images(
     @commands.bot_has_permissions(attach_files=True)
     @commands.max_concurrency(5, commands.BucketType.guild)
     async def iphonex(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Fits an image into an iPhone X screen.
 
@@ -492,7 +480,7 @@ class Images(
         ctx: SleepyContext,
         intensity: Annotated[int, Optional[commands.Range[int, 1, 10]]] = 5,
         *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        image: Annotated["PartialAsset", ImageAssetConverter],
     ) -> None:
         """JPEGifies an image to an optional intensity.
 
@@ -530,7 +518,7 @@ class Images(
         ctx: SleepyContext,
         colour: Annotated[Colour, Optional[Colour]] = Colour.red(),
         *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        image: Annotated["PartialAsset", ImageAssetConverter],
     ) -> None:
         """Places lensflares of a given colour on human eyes.
 
@@ -571,7 +559,7 @@ class Images(
         ctx: SleepyContext,
         intensity: Annotated[int, Optional[commands.Range[int, 1, 25]]] = 1,
         *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        image: Annotated["PartialAsset", ImageAssetConverter],
     ) -> None:
         """Heavily warps an image to an optional intensity.
 
@@ -619,10 +607,7 @@ class Images(
     @commands.bot_has_permissions(attach_files=True)
     @commands.max_concurrency(5, commands.BucketType.guild)
     async def palette(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Shows the five most prominent colours in an image.
 
@@ -775,10 +760,7 @@ class Images(
     @commands.bot_has_permissions(attach_files=True)
     @commands.max_concurrency(5, commands.BucketType.guild)
     async def soyjaks(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Generates a consoomer soyjaks pointing meme.
 
@@ -810,10 +792,7 @@ class Images(
     @commands.max_concurrency(2, commands.BucketType.guild)
     @commands.cooldown(1, 40, commands.BucketType.member)
     async def stickbug(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Generates a stickbug meme.
 
@@ -857,7 +836,7 @@ class Images(
         ctx: SleepyContext,
         intensity: Annotated[int, Optional[commands.Range[int, 1, 15]]] = 5,
         *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        image: Annotated["PartialAsset", ImageAssetConverter],
     ) -> None:
         """Swirls an image to an optional intensity.
 
@@ -953,10 +932,7 @@ class Images(
     @commands.bot_has_permissions(attach_files=True)
     @commands.max_concurrency(5, commands.BucketType.guild)
     async def threats(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Generates a "three threats to society" meme.
 
@@ -987,7 +963,7 @@ class Images(
         self,
         ctx: SleepyContext,
         title: Annotated[str, commands.clean_content(fix_channel_mentions=True)],
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        image: Annotated["PartialAsset", ImageAssetConverter],
         *,
         flavour_text: Annotated[str, commands.clean_content(fix_channel_mentions=True)],
     ) -> None:
@@ -1018,10 +994,7 @@ class Images(
     @commands.command()
     @commands.bot_has_permissions(attach_files=True)
     async def tucker(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Generates a live Tucker reaction meme.
 
@@ -1091,8 +1064,8 @@ class Images(
     async def whowouldwin(
         self,
         ctx: SleepyContext,
-        left_image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
-        right_image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        left_image: Annotated["PartialAsset", ImageAssetConverter],
+        right_image: Annotated["PartialAsset", ImageAssetConverter],
     ) -> None:
         """Generates a "who would win" meme.
 

sleepy/ext/nsfw.py
@@ -486,9 +486,7 @@ class NSFW(
         self,
         ctx: SleepyContext,
         *,
-        image: PartialAsset = commands.parameter(
-            converter=ImageAssetConverter(max_filesize=20_000_000),
-        ),
+        image: Annotated["PartialAsset", ImageAssetConverter(max_filesize=20_000_000)],
     ) -> None:
         """Reverse searches an image using SauceNAO.
 

sleepy/ext/web.py
@@ -1302,10 +1302,7 @@ class Web(
     @commands.command(aliases=("vredditdownloader",))
     @commands.bot_has_permissions(attach_files=True)
     async def vreddit(
-        self,
-        ctx: SleepyContext,
-        *,
-        url: str = commands.parameter(converter=RedditSubmissionURL),
+        self, ctx: SleepyContext, *, url: Annotated[str, RedditSubmissionURL]
     ) -> None:
         """Downloads a Reddit video submission.
 

sleepy/ext/weeb/frontend.py
@@ -199,10 +199,7 @@ class Weeb(
     @commands.command()
     @commands.cooldown(1, 8, commands.BucketType.member)
     async def animeface(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Detects and highlights anime faces in an image.
 
@@ -245,10 +242,7 @@ class Weeb(
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 5, commands.BucketType.member)
     async def awooify(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Awooifies an image.
 
@@ -276,10 +270,7 @@ class Weeb(
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 5, commands.BucketType.member)
     async def baguette(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Turns an image into an anime girl eating a baguette.
 
@@ -307,10 +298,7 @@ class Weeb(
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 5, commands.BucketType.member)
     async def bodypillow(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Turns an image into an anime body pillow.
 
@@ -429,10 +417,7 @@ class Weeb(
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 5, commands.BucketType.member)
     async def lolice(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Submits an image to the lolice.
 
@@ -571,10 +556,7 @@ class Weeb(
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 5, commands.BucketType.member)
     async def ritsudirt(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Generates a Ritsu dirt meme.
 
@@ -623,10 +605,7 @@ class Weeb(
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 5, commands.BucketType.member)
     async def trash(
-        self,
-        ctx: SleepyContext,
-        *,
-        image: PartialAsset = commands.parameter(converter=ImageAssetConverter),
+        self, ctx: SleepyContext, *, image: Annotated["PartialAsset", ImageAssetConverter]
     ) -> None:
         """Generates a trash waifu meme.
 

commit e74436289e997a5c676d45a5e4f85049d7b807e8

Fix `ban` command typehint.
Fixes an issue introduced by 4b57b4556466fe2db489565392f23d4ce735e6eb

sleepy/ext/moderation.py
@@ -283,8 +283,8 @@ class Moderation(commands.Cog):
     async def ban(
         self,
         ctx: GuildContext,
-        users: commands.Greedy[
-            Annotated[Union[discord.Member, discord.User], BannableUser]
+        users: Annotated[
+            Sequence[Union[discord.Member, discord.User]], commands.Greedy[BannableUser]
         ],
         clean_days: Annotated[int, Optional[_CleanDaysPsuedoFlag]] = 1,
         *,